366a1a5258fc518e9fe91939c8977b9cf319512e
sofia-mancini <sofia.mancini@uri.edu>
2025-07-10 11:19:06 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
-#include "Point.h"
+#include <string>
+#include <cmath>
+#include "Point2D.h"
 
-//No need to touch the main.cpp file. However - if might be helpfull
-//to look at how I'm using your soon-to-be defined Point.cpp class
 
-int main(){
+int main() {
+    Point myPt;
 
-    Point myPt(10, 5, 8);
+    myPt.setX(10);
+    myPt.setY(20);
+    myPt.setZ(30);
+
     Point myPt2(3, 10, 15);
 
-    int myX = myPt.getX();
-
-    std::cout << myX << std::endl;
-
-    std::cout << myPt.toCord() << std::endl;
-
-    myPt.setX(3);
-
-    std::cout << myPt.toCord() << std::endl;
-
-    std::cout << myPt.euDistance(myPt2) << std::endl;
-
-return 0;
 }
--- main_1.cpp
+++ main_1.cpp
@@ -1,35 +1,58 @@
-#include <cstdio>
-#include "bitmap_image.hpp"
+#include <iostream>
 
-int main()
-{
-   bitmap_image image("test09_color_map_image.bmp");
+struct Rectangle {
+    int width;
+    int height;
 
-   if (!image)
-   {
-      printf("Error - Failed to open: input.bmp\n");
-      return 1;
-   }
+    int area() {
+        return width * height;
+    }
+};
 
-   unsigned int total_number_of_pixels = 0;
+class Circle {
+    public:
+        int radius;
+        double area() {
+            return 3.14 * radius * radius;
+        }
+};
 
-   const unsigned int height = image.height();
-   const unsigned int width  = image.width();
+class Employee {
+    public:
+        Employee();
+        Employee(int id, std::string name, double salary);
+        void print();
+};
 
-   for (std::size_t y = 0; y < height; ++y)
-   {
-      for (std::size_t x = 0; x < width; ++x)
-      {
-         rgb_t colour;
+Employee::Employee() {
+    id = 0;
+    name = "";
+    salary = 0;
+}
 
-         image.get_pixel(x, y, colour);
+Employee::Employee(int id, std::string name, double salary) {
+    id = new_id;
+    name = newName;
+    salary = newSalary;
+}
 
-         if (colour.red >= 111)
-            total_number_of_pixels++;
-      }
-   }
+void Employee::print() {
+    std::cout << "ID: " << id << std::endl;
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Salary: " << salary << std::endl;
+}
 
-   printf("Number of pixels with red >= 111: %d\n",total_number_of_pixels);
 
-   return 0;
+int main() {
+    Rectangle r1;
+    r1.width = 10;
+    r1.height = 20;
+    std::cout << r1.area() << std::endl;
+
+    Circle c1;
+    c1.radius = 10;
+    std::cout << c1.area() << std::endl;
+
+    Employee e1;
+    e1.print();
 }
